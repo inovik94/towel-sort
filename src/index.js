8ca4d40723887c9b6a89342328cc1122a962b92b
“inovik94” <ilya.novikk@gmail.com>
2021-03-14 01:17:35 +0300

    refactor: update function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@
 
 module.exports = function towelSort (matrix) {
   let array = [];
+  if (matrix == 0 || matrix == undefined){
+    return []
+  }
   for (let i=0; i<matrix.length; i++) {
     if (i % 2 == 0) {
       matrix[i] = matrix[i].reverse();
